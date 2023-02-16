f440ab584b9d5335ab82decb194a64840f27c452
Gelix1470 <gleb.nazarov.1982@mail,ru>
2023-02-16 14:31:18 +0300

    newsave2

--- another_one/main.py
+++ another_one/main.py
@@ -1,5 +1,8 @@
-import html_creator as html
+import html_creator as hc
 import xml_generator as xg
+import data_provider as dp
 
-print(html.create())
-print(xg.create())
+# print(hc.create())
+# print(xg.create())
+
+hc.new_create(xg.new_create(dp.data_collection()))
